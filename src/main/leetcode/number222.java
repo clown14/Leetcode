3b5555616ced47a68c4d8dd156d2b909cbbfd769
clown14 <785613198@qq.com>
2019-10-02 10:48:02 +0800

    add 222

---
+++ src/main/leetcode/number222.java
@@ -0,0 +1,20 @@
+package main.leetcode;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-10-02 10:40
+ **/
+
+public class number222 {
+    class solution{
+        public int countNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return countNodes(root.left) + countNodes(root.right) + 1;
+        }
+    }
+}
+
+    
