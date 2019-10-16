d730c17ee53fd3cf95d620b02d234b8c28ec87d8
clown14 <785613198@qq.com>
2019-10-16 19:05:55 +0800

    add 455

---
+++ src/main/leetcode/greedy/number455.java
@@ -0,0 +1,39 @@
+package main.leetcode.greedy;
+
+import java.util.Arrays;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-10-16 19:02
+ **/
+
+/*
+根据题目，这是一个僧多粥少的事情，孩子多胃口大，要尽量满足更多的孩子，并且每个人都只能有一块饼干；
+
+因此，需要尽量满足胃口小的孩子，才能尽可能多人的满足；
+ */
+
+public class number455 {
+    class Solution {
+        public int findContentChildren(int[] g, int[] s) {
+            Arrays.sort(g);
+            Arrays.sort(s);
+            int count=0;
+            int j = 0;
+            for (int i = 0; i < g.length; i++) {
+                for (; j < s.length; j++) {
+                    if (s[j] >= g[i]) {
+                        j++;
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
+}
+
+    
