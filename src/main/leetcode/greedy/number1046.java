5eda7c871945216f9ba7b4312f9fcb63b0e97639
clown14 <785613198@qq.com>
2019-10-20 13:37:53 +0800

    add 1046

---
+++ src/main/leetcode/greedy/number1046.java
@@ -0,0 +1,26 @@
+package main.leetcode.greedy;
+
+import java.util.Arrays;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-10-20 13:35
+ **/
+
+public class number1046 {
+    class Solution {
+        public int lastStoneWeight(int[] stones) {
+            int len = stones.length;
+            for (int i = 0; i < len - 1; i++) {
+                Arrays.sort(stones);
+                int weight = stones[len - 1] - stones[len - 2];
+                stones[len - 1] = weight;
+                stones[len - 2] = 0;
+            }
+            return stones[len - 1];
+        }
+    }
+}
+
+    
