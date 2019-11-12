321c3d020d822bd8083f605fb8cfd753f9bf81bc
clown14 <785613198@qq.com>
2019-11-12 15:15:47 +0800

    add 101

---
+++ src/main/leetcode/tree/number101.java
@@ -0,0 +1,25 @@
+package main.leetcode.tree;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-11-12 15:12
+ **/
+
+public class number101 {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
+
+    
