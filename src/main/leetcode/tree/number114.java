ba77c1e4eb0e6ee5cab89b1ba15f36876f54b0ac
clown14 <785613198@qq.com>
2019-11-15 20:06:29 +0800

    add 114

---
+++ src/main/leetcode/tree/number114.java
@@ -0,0 +1,23 @@
+package main.leetcode.tree;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-11-15 20:04
+ **/
+
+public class number114 {
+    TreeNode pre = null;
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+    }
+}
+
+    
