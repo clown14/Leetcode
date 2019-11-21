f98f7b4cb3231796ad327e84ed5984cd14608d30
clown14 <785613198@qq.com>
2019-11-21 16:18:23 +0800

    add 145

---
+++ src/main/leetcode/tree/number145.java
@@ -0,0 +1,29 @@
+package main.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-11-21 16:15
+ **/
+
+public class number145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> post = new ArrayList<>();
+        posthelper(root, post);
+        return post;
+    }
+
+    private static void posthelper(TreeNode root, List<Integer> post) {
+        if (root == null) {
+            return;
+        }
+        posthelper(root.left, post);
+        posthelper(root.right, post);
+        post.add(root.val);
+    }
+}
+
+    
