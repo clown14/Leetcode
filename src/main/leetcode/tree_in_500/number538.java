0deaabcd06fa5c597d90c8e655780d9d56e30df2
clown14 <785613198@qq.com>
2019-12-02 11:48:26 +0800

    add 538

---
+++ src/main/leetcode/tree_in_500/number538.java
@@ -0,0 +1,27 @@
+package main.leetcode.tree_in_500;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-12-02 11:40
+ **/
+
+public class number538 {
+    int sum=0;
+    public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
+
+    private void convert(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        convert(root.right);
+        root.val += sum;
+        sum = root.val;
+        convert(root.left);
+    }
+}
+
+    
