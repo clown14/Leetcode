ddf7c82d6760e9c949c04b0bed6b702011ca6e5d
clown14 <785613198@qq.com>
2019-12-06 22:05:21 +0800

    add 572

---
+++ src/main/leetcode/tree_in_500/number572.java
@@ -0,0 +1,34 @@
+package main.leetcode.tree_in_500;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-12-06 21:29
+ **/
+
+public class number572 {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        if (isSame(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
+
+    
