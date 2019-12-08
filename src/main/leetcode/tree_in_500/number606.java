04255feac25dceae313abc1257bf8182b25991ed
clown14 <785613198@qq.com>
2019-12-08 17:55:20 +0800

    add 606

---
+++ src/main/leetcode/tree_in_500/number606.java
@@ -0,0 +1,24 @@
+package main.leetcode.tree_in_500;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-12-08 17:49
+ **/
+
+public class number606 {
+    public String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+        if (t.left == null && t.right == null) {
+            return t.val + "";
+        }
+        if (t.right == null) {
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) + ")";
+    }
+}
+
+    
