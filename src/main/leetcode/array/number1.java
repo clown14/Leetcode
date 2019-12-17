67af726d688b62487d2805959518e13439ae5dac
clown14 <785613198@qq.com>
2019-12-17 22:10:17 +0800

    add 1

---
+++ src/main/leetcode/array/number1.java
@@ -0,0 +1,24 @@
+package main.leetcode.array;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-12-17 22:09
+ **/
+
+public class number1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res=new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    res[0]=i;
+                    res[1]=j;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+    
--- src/main/leetcode/tree_in_700/number701.java
+++ src/main/leetcode/tree_in_700/number701.java
@@ -6,6 +6,10 @@ package main.leetcode.tree_in_700;
  * @create: 2019-12-16 16:11
  **/
 
+/*
+如果当前节点值>待插入节点值，则递归修改左子树，否则修改右子树。
+ */
+
 public class number701 {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root==null)  return new TreeNode(val);
