be8640803efd6d9d901fac63d74b89cb67344c3b
clown14 <785613198@qq.com>
2019-12-20 15:50:54 +0800

    add 11

---
+++ src/main/leetcode/array/number11.java
@@ -0,0 +1,28 @@
+package main.leetcode.array;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2019-12-20 15:43
+ **/
+
+/*
+我们在由线段长度构成的数组中使用两个指针，一个放在开始，一个置于末尾。 此外，我们会使用变量 [公式] 来持续存储到目前为止所获得的最大面积。
+在每一步中，我们会找出指针所指向的两条线段形成的区域，更新 [公式]，并将指向较短线段的指针向较长线段那端移动一步。
+ */
+public class number11 {
+    public int maxArea(int[] height) {
+        int maxArea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
+
+    
