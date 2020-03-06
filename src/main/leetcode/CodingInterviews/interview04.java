1dff120d970283f6164502f4af73d95607825b3b
clown14 <785613198@qq.com>
2020-03-06 16:21:55 +0800

    add interview04

---
+++ src/main/leetcode/CodingInterviews/interview04.java
@@ -0,0 +1,34 @@
+package main.leetcode.CodingInterviews;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2020-03-06 16:17
+ **/
+
+/**
+ * 从二维数组的右上角开始查找。如果当前元素等于目标值，则返回 true。如果当前元素大于目标值，则移到左边一列。如果当前元素小于目标值，则移到下边一行。
+ */
+
+public class interview04 {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int row = 0, column = columns - 1;
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
+
+    
