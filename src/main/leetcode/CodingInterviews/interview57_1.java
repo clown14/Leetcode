0a2ab2a0a20597893bb89a13851a3b833beac8a4
clown14 <785613198@qq.com>
2020-05-08 15:52:51 +0800

    add interview57 & 1

---
+++ src/main/leetcode/CodingInterviews/interview57_1.java
@@ -0,0 +1,30 @@
+package main.leetcode.CodingInterviews;
+
+/**
+ * @program: Leetcode
+ * @author: onion
+ * @create: 2020-05-08 15:47
+ **/
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 在进行迭代并将元素插入到表中的同时，我们还会回过头来检查表中是否已经存在当前元素所对应的目标元素。如果它存在，那我们已经找到了对应解，并立即将其返回。
+ */
+
+public class interview57_1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+    
