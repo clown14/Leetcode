4be48306bb9e927d08b3a0ab91c53187bad9d1aa
clown14 <785613198@qq.com>
2020-06-10 15:06:54 +0800

    update 98 栈

--- src/main/leetcode/tree_in_250/number98.java
+++ src/main/leetcode/tree_in_250/number98.java
@@ -1,30 +1,58 @@
 package main.leetcode.tree_in_250;
 
+import java.util.Stack;
+
 /**
  * @program: Leetcode
  * @author: onion
  * @create: 2019-09-25 22:52
  **/
-
 public class number98 {
-    class solution {
-        public boolean isValidBST(TreeNode root) {
-            if (root == null) {
-                return true;
-            }
-            return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
-        }
 
-        private boolean valid(TreeNode root, long low, long high) {
-            if (root == null) {
-                return true;
+    /**
+     * 中序遍历递归
+     */
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean valid(TreeNode root, long low, long high) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val < low || root.val > high) {
+            return false;
+        }
+        return valid(root.left, low, root.val) && valid(root.right, root.val, high);
+    }
+
+    /**
+     * 栈
+     */
+    public boolean isValidBST2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root, pre = null;
+        while (p != null || !stack.empty()) {
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
             }
-            if (root.val < low || root.val > high) {
+            p = stack.pop();
+            if (pre != null && p.val <= pre.val) {
                 return false;
             }
-            return valid(root.left, low, root.val) && valid(root.right, root.val, high);
+            pre = p;
+            p = p.right;
         }
+        return true;
     }
 }
+
+
+
 
     
