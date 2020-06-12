f919ed65e0859bf34b06b0f975d132a6bdde1bb1
clown14 <785613198@qq.com>
2020-06-12 16:09:50 +0800

    update 100 迭代

--- src/main/leetcode/tree_in_250/number100.java
+++ src/main/leetcode/tree_in_250/number100.java
@@ -1,5 +1,8 @@
 package main.leetcode.tree_in_250;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @program: Leetcode
  * @author: onion
@@ -7,6 +10,7 @@ package main.leetcode.tree_in_250;
  **/
 
 public class number100 {
+    //递归
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null&&q==null)
             return true;
@@ -15,6 +19,27 @@ public class number100 {
         if(p.val!=q.val)
             return false;
         return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+
+    //迭代
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while (!queue.isEmpty()) {
+            TreeNode f = queue.poll();
+            TreeNode s = queue.poll();
+            if (f == null && s == null) {
+                continue;
+            } else if (f == null || s == null || s.val != f.val) {
+                return false;
+            }
+            queue.add(f.left);
+            queue.add(s.left);
+            queue.add(f.right);
+            queue.add(s.right);
+        }
+        return true;
     }
 }
 
