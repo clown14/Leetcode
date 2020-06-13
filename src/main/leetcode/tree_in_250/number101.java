701670c326c7eee96322ee175a72bf85019b89f4
clown14 <785613198@qq.com>
2020-06-13 15:58:46 +0800

    update 101 迭代

--- src/main/leetcode/tree_in_250/number101.java
+++ src/main/leetcode/tree_in_250/number101.java
@@ -1,5 +1,8 @@
 package main.leetcode.tree_in_250;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @program: Leetcode
  * @author: onion
@@ -19,6 +22,31 @@ public class number101 {
             return false;
         }
         return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+    /**
+     * 迭代
+     * 最初，队列中包含的是 root 以及 root。该算法的工作原理类似于 BFS，但存在一些关键差异。每次提取两个结点并比较它们的值。
+     * 然后，将两个结点的左右子结点按相反的顺序插入队列中。当队列为空时，或者我们检测到树不对称（即从队列中取出两个不相等的连续结点）时，该算法结束
+     */
+    private boolean isMirror2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            } else if (t1 == null || t2 == null || t1.val != t2.val) {
+                return false;
+            }
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        return true;
     }
 }
 
