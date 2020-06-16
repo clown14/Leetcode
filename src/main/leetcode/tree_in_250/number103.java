91da2b076356b7700bed58b49a8649ec65232d9a
clown14 <785613198@qq.com>
2020-06-16 22:33:04 +0800

    update 103 递归

--- src/main/leetcode/tree_in_250/number103.java
+++ src/main/leetcode/tree_in_250/number103.java
@@ -1,6 +1,7 @@
 package main.leetcode.tree_in_250;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -10,13 +11,13 @@ import java.util.Stack;
  * @create: 2019-09-23 20:01
  **/
 
-/*
-建立两个栈stack1和stack2
-把二叉树的根节点push进stack1
-使用一个while循环，pop当前节点的子结点，然后push进另一个栈，这样每处理一个栈，就在最终结果ans里面加一个列表（当前深度的节点
- */
 
 public class number103 {
+    /*
+    建立两个栈stack1和stack2
+    把二叉树的根节点push进stack1
+    使用一个while循环，pop当前节点的子结点，然后push进另一个栈，这样每处理一个栈，就在最终结果ans里面加一个列表（当前深度的节点
+     */
     public List<List<Integer>> zigzagLevelOrder (TreeNode root) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         if (root == null) {
@@ -55,6 +56,35 @@ public class number103 {
             }
         }
         return ans;
+    }
+
+    //递归
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        helper(res, root, 0);
+        return res;
+    }
+
+    public void helper(List<List<Integer>> res,TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        // 注意这里new List, 说明当前节点递归进入了新的层.
+        if (res.size() <= depth) {
+            res.add(new LinkedList<>());
+        }
+
+        if (depth % 2 != 0) {
+            res.get(depth).add(0, root.val);
+        } else {
+            res.get(depth).add(root.val);
+        }
+
+        helper(res, root.left, depth + 1);
+        helper(res, root.right, depth + 1);
     }
 }
 
