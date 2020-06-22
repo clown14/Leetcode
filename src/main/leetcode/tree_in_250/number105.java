5f4eed7c0e268edbd15b813c3f830753816adcfa
clown <785613198@qq.com>
2020-06-22 16:23:31 +0800

    update 105 迭代

--- src/main/leetcode/tree_in_250/number105.java
+++ src/main/leetcode/tree_in_250/number105.java
@@ -2,6 +2,7 @@ package main.leetcode.tree_in_250;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * @program: Leetcode
@@ -10,6 +11,11 @@ import java.util.Map;
  **/
 
 public class number105 {
+    /**
+     * 递归
+     * 1、前序遍历序列第一个元素一定是该树的根节点。
+     * 2、中序遍历序列中根节点前面一定是该树的左子树，后面是该树的右子树。
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder.length==0 || inorder.length==0) {
             return null;
@@ -41,6 +47,51 @@ public class number105 {
 
         return node;
     }
+
+    /**
+     * 迭代
+     * 前序遍历是 中 - 左 - 右 中序遍历是 左- 中 - 右 遍历前序序列并入栈，直到遍历的数值和中序遍历的首个元素相同时，
+     * 意味着根节点左侧节点添加完成，出栈到根节点，这时候前序的节点是当前根节点的右侧节点。循环执行上边的操作，完成树的添加。
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) return null;
+
+        Stack<TreeNode> roots = new Stack<TreeNode>();
+        int pre = 0;
+        int in = 0;
+        //先序遍历第一个值作为根节点
+        TreeNode curRoot = new TreeNode(preorder[pre]);
+        TreeNode root = curRoot;
+        roots.push(curRoot);
+        pre++;
+        //遍历前序遍历的数组
+        while (pre < preorder.length) {
+            //出现了当前节点的值和中序遍历数组的值相等，表示左子树添加完成，根据压栈找到对应的根节点，
+            // curRoot指向新的根节点，添加右边的节点
+            if (curRoot.val == inorder[in]) {
+                //每次进行出栈，实现倒着遍历
+                while (!roots.isEmpty() && roots.peek().val == inorder[in]) {
+                    //curRoot = roots.peek();
+                    curRoot = roots.pop();
+                    in++;
+                }
+                //设为当前的右孩子
+                curRoot.right = new TreeNode(preorder[pre]);
+                //更新 curRoot
+                curRoot = curRoot.right;
+                roots.push(curRoot);
+                pre++;
+            } else {
+                //否则的话就一直作为左子树
+                curRoot.left = new TreeNode(preorder[pre]);
+                curRoot = curRoot.left;
+                roots.push(curRoot);
+                pre++;
+            }
+        }
+        return root;
+    }
+
 }
 
     
